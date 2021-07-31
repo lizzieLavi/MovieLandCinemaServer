dce471b3438607a98f596ae750990a8282a0e9b1
unknown <lizzie.olishuk1@gmail.com>
2021-07-31 11:27:00 +0300

    try

--- configs/database.js
+++ configs/database.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/CinemaWS');
+mongoose.connect('mongodb://lizzieLavi:olishuk2089@cinemadb-shard-00-00.j1yph.mongodb.net:27017,cinemadb-shard-00-01.j1yph.mongodb.net:27017,cinemadb-shard-00-02.j1yph.mongodb.net:27017/CinemaWS?ssl=true&replicaSet=atlas-rdjdpv-shard-0&authSource=admin&retryWrites=true&w=majority');
